68de2883b860f7e899f761a36715d2728d9734b6
anitgandhi <anitgandhi@gmail.com>
2015-01-25 16:42:03 -0600

    more small fixes on resume upload

--- resumeUpload.php
+++ resumeUpload.php
@@ -8,20 +8,19 @@ if ($_POST and $_FILES)
 {
 	$resumeName = $_POST["resumeName"];
 	$resumeNetID = $_POST["resumeNetID"];
-	echo $resumeName . " - " . $resumeNetID . " - Resume";
 
 	try
 	{
 		// raise exception on upload error
 	    if ( ($_FILES['resume']['error'] !== UPLOAD_ERR_OK) || ($_FILES['resume']['name'] === "") )
-	        throw new Exception("Resume did not upload properly.");
+	        throw new Exception("Resume did not upload properly. Please try again.");
 	
 	    // parse the extension from the file name
 	    $ext = substr($_FILES['resume']['name'], strrpos($_FILES['resume']['name'], '.') + 1);
 	
 	    // raise exception if the file type is wrong
 	    if ( ($ext != "pdf") && ($ext != "doc") && ($ext != "docx") )
-	        throw new Exception("File type is not PDF or Microsoft Word document.");
+	        throw new Exception("File type is not PDF or Microsoft Word document. Please try again.");
 	
 	    // get the file name based on student's information
 	    $filename = $resumeName . " - " . $resumeNetID . " - Resume." . $ext;
@@ -31,13 +30,13 @@ if ($_POST and $_FILES)
 	    // upload to the "resume" folder (if the folder doesn't exist, it will be created)
 	    $uploader->upload($_FILES['resume']['tmp_name'], "SPAC 2015 Resumes",  $filename);
 	
-	    echo "success";
+	    echo "Your resume has been uploaded.";
 	}
 
 	catch(Exception $e)
 	{
         // exception raised
-        echo htmlspecialchars($e->getMessage());
+        echo $e->getMessage();
     }
 
 }
